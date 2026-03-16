395f3139de28e9db21d77c21966ce32a87f3ca9c
aayanfakki2 <aayanfakki2@gmail.com>
2026-03-16 16:48:56 +0530

    Create FUNCTION.C

---
+++ FUNCTION.C
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int add(int a, int b );
+int sub(int a, int b );
+int mul(int a, int b );
+int factorial(int n);
+float divi(int a, int b);
+
+int main(){
+    int choice ,a,b,n;
+    do{
+       printf('\n---menu---\n');
+       printf('1.Addition\n');
+       printf('2.subtraction\n');
+       printf('3.multiplication\n');
+       printf('4.division\n');
+       printf('5.factorial\n');
+       printf('5.exit\n');
+       printf('Enter Your choice\n');
+       scanf()
+
+       
