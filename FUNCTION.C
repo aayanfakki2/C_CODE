3203c7a597c2f21cdcb894081cd42d13b1490ca1
aayanfakki2 <aayanfakki2@gmail.com>
2026-03-16 17:01:27 +0530

    Update FUNCTION.C

--- FUNCTION.C
+++ FUNCTION.C
@@ -16,6 +16,49 @@ int main(){
        printf('5.factorial\n');
        printf('5.exit\n');
        printf('Enter Your choice\n');
-       scanf()
+       scanf("%d , &choice");
+
+
+switch(choice)
+        {
+            case 1:
+                printf("Enter two numbers: ");
+                scanf("%f %f", &a, &b);
+                printf("Result = %.2f\n", add(a,b));
+                break;
+            case 2:
+                printf("Enter two numbers: ");
+                scanf("%f %f", &a, &b);
+                printf("Result = %.2f\n", sub(a,b));
+                break;
+            case 3:
+                printf("Enter two numbers: ");
+                scanf("%f %f", &a, &b);
+                printf("Result = %.2f\n", multiply(a,b));
+                break;
+            case 4:
+                printf("Enter two numbers: ");
+                scanf("%f %f", &a, &b);
+                if(b != 0)
+                    printf("Result = %.2f\n", divide(a,b));
+                else
+                    printf("Division by zero is not allowed\n");
+                break;
+            case 5:
+                printf("Enter a number: ");
+                scanf("%d", &num);
+                printf("Factorial = %d\n", factorial(num));
+                break;
+            case 6:
+                printf("Exiting program...\n");
+                return 0;
+            default:
+                printf("Invalid choice!\n");
+        }
+    }
+}
+
+
+
 
        
